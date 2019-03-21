06ccd10391346f453d7af3a4a479597bc265e965
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 03:29:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
+  current_line = []
   if katz_deli.length < 1 
     puts "The line is currently empty."
   elsif katz_deli >= 1 
-  attendees.each_with_index.map do |attendee, index|
+  attendees.each_with_index do |index, name|
   end
 end
