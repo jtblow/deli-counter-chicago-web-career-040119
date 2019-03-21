38a717da87969c20689dbf7c33c49b98f5e555fb
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 04:38:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ end
 puts current_line
 end
 end
-def take_a_number(name)
+def take_a_number(katz_deli,name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
