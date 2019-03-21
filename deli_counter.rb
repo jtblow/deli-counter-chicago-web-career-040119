635d61891113f7024a4aed597e4668058564df64
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 03:41:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.length < 1 
     puts "The line is currently empty."
   else
-  katz_deli.each_with_index.map |name, index|
+  katz_deli.each_with_index.map do |name, index|
    "The line is currently: #{index + 1} #{name}"
 end
 end
