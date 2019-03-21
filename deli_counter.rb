7bb280342a2568007a8362aa6b8f33ce4e4fa2c7
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 03:37:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.length < 1 
     puts "The line is currently empty."
   else
-  katz_deli.each_with_index do |index, name|
+  katz_deli.each_with_index do |name, index|
    puts current_line << "The line is currently: #{index + 1} #{name}"
 end
 end
