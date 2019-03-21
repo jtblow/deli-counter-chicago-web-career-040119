06814c29a28d5de22444e85cac57584052381419
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 04:37:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
-def now_serving(name)
+def now_serving(na)
   if katz_deli.length < 1 
     puts "There is nobody waiting to be served!"
   else
