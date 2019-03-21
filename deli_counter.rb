b2e50a4383ef6dfc2ed470d5a5ecf7bc86e0e2a6
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 04:38:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def now_serving(katz_deli)
   if katz_deli.length < 1 
     puts "There is nobody waiting to be served!"
   else
- katz_deli.shift(name)
- puts "Currently serving #{name}."
+ puts "Currently serving #{katz_deli.first}."
+ katz_deli.shift()
 end
 end
