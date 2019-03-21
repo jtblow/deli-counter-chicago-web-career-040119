abfce2ee33c7c6c482f4ae69cb6e19b9081ff022
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 03:33:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   current_line = []
   if katz_deli.length < 1 
     puts "The line is currently empty."
-  elsif katz_deli >= 1 
+  else
   attendees.each_with_index do |index, name|
     current_line << "The line is currently: #{index} #{name}"
   end
